6155aedda982e4da71d98ffaec1da69d041f6669
Quan Nguyen <quanuw@gmail.com>
2016-04-01 14:28:05 -0700

    initial commit

---
+++ currencyCalc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main() {
+	setbuf(stdout, NULL);
+	int whatToDo = 0;
+	float money = 0;
+	float exchangeRate = 0;
+	printf("\n");
+	printf("Enter an option.\n");
+	printf("1. convert dollars to foreign currency.\n");
+	printf("2. convert foreign currency to dollars.\n");
+	printf("Please enter an option: ");
+	scanf(" %d", &whatToDo);
+	if (whatToDo == 1) {
+		printf("Enter amount in money: ");
+		scanf(" %f", &money);
+		printf("What is the exchange rate? ");
+		scanf(" %f", &exchangeRate);
+		printf("%.2f equals %.2f in the foreign currency.\n", money, money * exchangeRate);
+	} else if (whatToDo == 2) {
+		printf("Enter amount in foreign currency: ");
+		scanf(" %f", &money);
+		printf("What is the exchange rate? ");
+		scanf(" %f", &exchangeRate);
+		printf("%.2f equals %.2f in US dollars.\n", money, money * exchangeRate);
+	} else {
+		printf("Please choose option 1 or 2.\n");
+	}
+}
