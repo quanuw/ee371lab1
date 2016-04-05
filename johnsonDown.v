a9dde3a2489b160925e4cfa63199602636d434e0
bradleymarquez <bradleymarquez@hotmail.com>
2016-04-04 17:45:49 -0700

    Added files via upload

--- johnsonDown.v
+++ johnsonDown.v
@@ -1,7 +1,7 @@
 module johnsonDown(input clk, input reset, output [3:0] signal);
 	reg [3:0] temp;
 	always @ (posedge clk) begin
-		if (reset) begin
+		if (~reset) begin
 			temp <= 4'b0;
 		end else if (signal[0]) begin
 			temp <= {1'b0, signal[3:1]};
